1a1a8bc23223d35701a6cc00b54efada1faa7f6a
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-03-03 15:31:37 +0200

    tSK-3 update

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,27 @@
+class StringBuilder {
+  
+#value;
+constructor(value){
+ this.#value = value;
+}
 
+getValue() {
+    return this.#value;
+}
+
+padStart(str){
+   return this.#value = str.concat(this.#value);
+}
+
+padEnd(str){
+return this.#value = this.#value.concat(str);
+}
+
+padBoth(str){
+ return this.#value = str.concat(this.#value, str);
+}
+
+}
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
